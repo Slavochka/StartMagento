b735976429b105dd6493cc0bd449daba4ad97ecc
Slavochka <Slavochk@bk.ru>
2016-07-07 10:29:05 +0300

    11 steps was passed

---
+++ app/code/local/Help/Faq/Block/Adminhtml/Faq/Grid.php
@@ -0,0 +1,55 @@
+<?php
+
+class Help_Faq_Block_Adminhtml_Faq_Grid extends Mage_Adminhtml_Block_Widget_Grid
+{
+
+    protected function _prepareCollection()
+    {
+        $collection = Mage::getModel('helpfaq/faq')->getCollection();
+        $this->setCollection($collection);
+        return parent::_prepareCollection();
+    }
+
+    protected function _prepareColumns()
+    {
+
+        $helper = Mage::helper('helpfaq');
+
+        $this->addColumn('faq_id', array(
+            'header' => $helper->__('Faq ID'),
+            'index' => 'faq_id'
+        ));
+
+        $this->addColumn('name', array(
+            'header' => $helper->__('Name'),
+            'index' => 'name',
+            'type' => 'text',
+        ));
+
+        $this->addColumn('email', array(
+            'header' => $helper->__('Email'),
+            'index' => 'email',
+            'type' => 'text',
+        ));
+
+        $this->addColumn('created', array(
+            'header' => $helper->__('Created'),
+            'index' => 'created',
+            'type' => 'date',
+        ));
+
+        return parent::_prepareColumns();
+    }
+
+    protected function _prepareMassaction()
+    {
+        $this->setMassactionIdField('faq_id');
+        $this->getMassactionBlock()->setFormFieldName('faq');
+
+        $this->getMassactionBlock()->addItem('delete', array(
+            'label' => $this->__('Delete'),
+            'url' => $this->getUrl('*/*/massDelete'),
+        ));
+        return $this;
+    }
+}
--- app/code/local/Help/Faq/controllers/Adminhtml/FaqController.php
+++ app/code/local/Help/Faq/controllers/Adminhtml/FaqController.php
@@ -4,6 +4,30 @@ class Help_Faq_Adminhtml_FaqController extends Mage_Adminhtml_Controller_Action
 {
     public function indexAction()
     {
-        echo '<h1>FAQ Module: Admin section</h1>';
+        $this->loadLayout();
+        $this->_setActiveMenu('helpfaq');
+
+        $contentBlock = $this->getLayout()->createBlock('helpfaq/adminhtml_faq');
+        $this->_addContent($contentBlock);
+        $this->renderLayout();
+    }
+
+    public function massDeleteAction()
+    {
+        $faq = $this->getRequest()->getParam('faq', null);
+
+        if (is_array($faq) && sizeof($faq) > 0) {
+            try {
+                foreach ($faq as $id) {
+                    Mage::getModel('helpfaq/faq')->setId($id)->delete();
+                }
+                $this->_getSession()->addSuccess($this->__('Total of %d faq have been deleted', sizeof($faq)));
+            } catch (Exception $e) {
+                $this->_getSession()->addError($e->getMessage());
+            }
+        } else {
+            $this->_getSession()->addError($this->__('Please select faq'));
+        }
+        $this->_redirect('*/*');
     }
 }
